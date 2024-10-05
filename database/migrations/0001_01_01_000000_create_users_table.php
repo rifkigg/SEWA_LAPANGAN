f7e7218c507aef5daa6a5b0e15999090df9b3595
rifkigg <rifkiadi348@gmail.com>
2024-10-05 10:34:18 +0700

    menambahkan breeze

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('avatar')->nullable();
+            $table->string('username');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role' ,['admin','user', 'owner'])->default('user');
             $table->rememberToken();
             $table->timestamps();
         });
