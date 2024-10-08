eea3088f6c86d77e77a725bc34d5216612b86eb3
rifkigg <rifkiadi348@gmail.com>
2024-10-08 13:31:44 +0700

    mengubah timezone jadi asia/jakarta

--- config/session.php
+++ config/session.php
@@ -32,7 +32,7 @@ return [
     |
     */
 
-    'lifetime' => env('SESSION_LIFETIME', 120),
+    'lifetime' => env('SESSION_LIFETIME', 1440), // Mengubah lifetime menjadi 24 jam (1440 menit)
 
     'expire_on_close' => env('SESSION_EXPIRE_ON_CLOSE', false),
 
